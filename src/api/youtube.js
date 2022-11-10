5f8c2d738c3839969be60d06b9eaf454be90dfa2
hsjung2015 <hsjung201558@gmail.com>
2022-11-10 15:37:59 +0900

    api 만들기

---
+++ src/api/youtube.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export default class Youtube {
+    constructor() {
+        this.httpClient = axios.create({})
+    }
+
+    async search(keyword){
+        return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
+    }
+    async #searchByKeyword(keyword) {
+        return axios
+        .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+        .then((res) => res.data.items)
+        .then((items) => items.map((item) => ({ ...item, id: item.id.videoId})))
+    }
+    
+    async #mostPopular() {
+        return axios.get(`/videos/popular.json`).then((res) => res.data.items);
+    }
+
+}
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import axios from 'axios';
+import { search } from '../api/youtube';
+import FakeYoutube from '../api/fakeYoutube';
 
 export default function Videos() {
   const { keyword } = useParams();
@@ -11,8 +12,8 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(['videos', keyword], async () => {
-    return axios.get(`/videos/${keyword ? 'search' : 'popular'}.json`)
-      .then((res) => res.data.items);
+    const youtube = new FakeYoutube();
+    return youtube.search(keyword);
   });
   return (
     <>
