e429cf9d42b9f1055326088ad8008bfba10cb043
hsjung2015 <hsjung201558@gmail.com>
2022-11-10 17:50:58 +0900

    api 작업

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,21 +2,35 @@ import axios from 'axios';
 
 export default class Youtube {
     constructor() {
-        this.httpClient = axios.create({})
+        this.httpClient = axios.create({
+            baseURL: 'https://www.googleapis.com/youtube/v3',
+            params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+        })
     }
 
     async search(keyword){
         return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
     }
     async #searchByKeyword(keyword) {
-        return axios
-        .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+        return this.httpClient
+        .get('search', {params: {
+            part: 'snnipet',
+            maxResults: 25,
+            type: 'video',
+            q: keyword
+        }})
         .then((res) => res.data.items)
         .then((items) => items.map((item) => ({ ...item, id: item.id.videoId})))
     }
     
     async #mostPopular() {
-        return axios.get(`/videos/popular.json`).then((res) => res.data.items);
+        return this.httpClient
+        .get('videos', {params: {
+            part: 'snnipet',
+            maxResults: 25,
+            chart: 'mostPopular'
+        }})
+        
     }
 
 }
